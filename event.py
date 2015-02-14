3d5347e3568b2a639b003495e441a88ac84f9b00
Chris Whitworth <chris.whitworth@gmail.com>
2015-02-14 12:57:13 +0000

    Maps and links

--- event.py
+++ event.py
@@ -13,8 +13,8 @@ class RecurringEvent(object):
         self.recurrence = recurrence
 
     def as_csv(self):
-        line = '"{0}","{1}","{2}","{3}","{4}","{5}","{6}","{7}"\n'
-        output = '"Event title","Start time","Start date","End time","End date","Location","Category","Description"\n'
+        line = '"{0}","{1}","{2}","{3}","{4}","{5}","{6}",true,true,"{7}"\n'
+        output = '"Event title","Start time","Start date","End time","End date","Location","Category","Maps","Map link","Description"\n'
         count = 1
         for date in self.recurrence.dates():
             name = self.event.name.format(count)
